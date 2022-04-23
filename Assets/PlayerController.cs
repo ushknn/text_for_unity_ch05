6628c3bd34ada68635577c304dc8f6f596992c29
ushknn <uekiworks@gmail.com>
2022-04-23 16:48:23 +0900

    add project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.LeftArrow))
+    //     {
+    //         transform.Translate(-3, 0, 0);
+    //     }
+
+    //     if (Input.GetKeyDown(KeyCode.RightArrow))
+    //     {
+    //         transform.Translate(3, 0, 0);
+    //     }
+    // }
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+}
